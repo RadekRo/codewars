9e8d3dbe9dfe12482ef86239eb32ba3af0e034a9
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-10 13:57:00 +0100

    First task completed :)

--- python.py
+++ python.py
@@ -1,3 +1,16 @@
 # These are the solutions of the codewars.com challenges in Python language
 
+# Challenge 1 - 7 kyu
+# check if the provided number is a perfect square
 
+def is_square(n):  
+    if n < 0:
+        return False
+    else:
+        calc_base = n ** 0.5
+        return calc_base - int(calc_base) == 0
+
+print(is_square(-1))
+print(is_square(0))
+print(is_square(4))
+print(is_square(7))
