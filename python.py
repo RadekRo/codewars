5dbcf693fcaf0091558bdd960e1b55a839dde68b
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-10 14:14:19 +0100

    Second task completed

--- python.py
+++ python.py
@@ -10,7 +10,27 @@ def is_square(n):
         calc_base = n ** 0.5
         return calc_base - int(calc_base) == 0
 
-print(is_square(-1))
-print(is_square(0))
-print(is_square(4))
-print(is_square(7))
+# TESTS
+# print(is_square(-1))
+# print(is_square(0))
+# print(is_square(4))
+# print(is_square(7))
+
+# Challenge 2 - 7 kyu
+# find a sum of provided TWO numbers and ALL between them. Numbers given unorderer.
+
+def get_sum(a,b):
+    if a == b:
+        return a
+    else:
+        arr = [a, b]
+        sum = 0
+        arr.sort()
+        for i in range(arr[0], arr[1]+1):
+            sum += i
+        return sum
+    
+#TESTS
+#print(get_sum(2,2))
+#print(get_sum(-1,0))
+#print(get_sum(5,3))
