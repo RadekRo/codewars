c615601bb20e9f450931d7edec58c0d53503fc4d
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-12 11:59:09 +0100

    Challenge nr 8 completed

--- python.py
+++ python.py
@@ -122,3 +122,24 @@ def duplicate_encode(word):
         else:
             coded_word.append("(")
     return "".join(coded_word) 
+
+# favourite solution:
+# return "".join(["(" if word.lower().count(c) == 1 else ")" for c in word.lower()])
+
+# Challenge 8 - 8 kyu
+
+# Complete the function so that it finds the average of the three scores passed 
+# to it and returns the letter value associated with that grade.
+
+def get_grade(s1, s2, s3):
+    score = (s1+s2+s3)/3
+    if 90 <= score <= 100:
+        return 'A'
+    elif 80 <= score < 90:
+        return 'B'
+    elif 70 <= score < 80:
+        return 'C'
+    elif 60 <= score < 70:
+        return 'D'
+    elif 0 <= score < 60:
+        return 'F'
