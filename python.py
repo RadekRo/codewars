148b96c9b2ec6adb8273ada2762b3fe048dcee18
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-12 12:35:37 +0100

    Challenge nr 9 - completed. Gained total 232 xp, 6 kyu

--- python.py
+++ python.py
@@ -143,3 +143,42 @@ def get_grade(s1, s2, s3):
         return 'D'
     elif 0 <= score < 60:
         return 'F'
+
+# favourite solution:
+# return {6:'D', 7:'C', 8:'B', 9:'A', 10:'A'}.get((s1 + s2 + s3) / 30, 'F')
+
+# Challenge 9 - 6 kyu
+# You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+# You arrived ten minutes too early to an appointment, so you decided to take the 
+# opportunity to go for a short walk. The city provides its citizens with a Walk 
+# Generating App on their phones -- everytime you press the button it sends you an 
+# array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). 
+# You always walk only a single block for each letter (direction) and you know it takes 
+# you one minute to traverse one city block, so create a function that will return true 
+# if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) 
+# and will, of course, return you to your starting point. Return false otherwise.
+
+def is_valid_walk(walk):
+    x_axis = 0
+    y_axis = 0
+    for coord in walk:
+        match coord:
+            case "n": 
+                y_axis += 1
+            case "s":
+                y_axis -= 1
+            case "e":
+                x_axis += 1
+            case "w":
+                x_axis -= 1
+            case _: 
+                print("Wrong coordinates entered!")
+                break
+    return x_axis == 0 and y_axis == 0 and len(walk) == 10
+
+#TESTS
+# print(is_valid_walk(["n", "e", "n", "e", "s", "w", "s", "w", "e", "w"]))
+# print(is_valid_walk(["n", "e", "n", "e", "s", "w", "s", "w", "e", "w", "e", "w"]))
+# print(is_valid_walk(["n", "e"]))
+# favourite solution:
+# return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
